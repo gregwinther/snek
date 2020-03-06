9a90e70e42caa68a9129c6d0d0c4556032f0fcef
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-03-06 11:59:01 +0100

    Add game logic

--- snake_game/state.py
+++ snake_game/state.py
@@ -1,5 +1,5 @@
 import numpy as np
-from random import randint
+from random import randint, choice
 
 
 BORDER = 1
@@ -8,11 +8,15 @@ HEAD = 3
 FOOD = -1
 
 
+ACTIONS = dict(FORWARD=0, LEFT=-1, RIGHT=1)
+
+
 class State:
     def __init__(self, board_width=20, board_height=20, initial_length=3):
         assert (board_height - 2) - initial_length > 0 and (
             board_width - 2
         ) - initial_length > 0
+        assert initial_length > 1
 
         self.board_height = board_height
         self.board_width = board_width
@@ -26,6 +30,7 @@ class State:
         self.food = self.place_food()
 
         self.done = False
+        self.score = 0
 
     @staticmethod
     def init_board(width, height):
@@ -87,9 +92,76 @@ class State:
 
         return point
 
+    def step(self, action):
+        assert action in ACTIONS.values()
+
+        if self.done:
+            return self
+
+        self._move_snake_head(action)
+        self._check_collisions()
+
+        if self._food_eaten():
+            self.score += 1
+            self.place_food()
+        else:
+            self._remove_snake_tail()
+
+        self.place_snake()
+
+        return self
+
+    def _move_snake_head(self, action):
+        # XXX: Exercise
+        i_0, j_0 = self.snake[0]
+        i_1, j_1 = self.snake[1]
+
+        delta_i, delta_j = i_0 - i_1, j_0 - j_1
+
+        if action == ACTIONS["LEFT"]:
+            delta_i, delta_j = self._turn_left(delta_i, delta_j)
+        elif action == ACTIONS["RIGHT"]:
+            delta_i, delta_j = self._turn_right(delta_i, delta_j)
+
+        new_head = [i_0 + delta_i, j_0 + delta_j]
+        self.snake.insert(0, new_head)
+
+    def _turn_left(self, delta_i, delta_j):
+        # XXX: Exercise
+        return delta_j, -delta_i
+
+    def _turn_right(self, delta_i, delta_j):
+        # XXX: Exercise
+        return -delta_j, delta_i
+
+    def _check_collisions(self):
+        # XXX: Exercise
+        i_0, j_0 = self.snake[0]
+        if self.board[i_0][j_0] > 0:
+            self.done = True
+
+    def _food_eaten(self):
+        # XXX: Exercise
+        return self.snake[0] == self.food
+
+    def _remove_snake_tail(self):
+        # XXX: Exercise
+        i, j = self.snake.pop()
+        self.board[i][j] = 0
+
 
 if __name__ == "__main__":
     # Test initialization
     state = State(board_width=6, board_height=6, initial_length=3)
+    print("\033c", end="")
     print(np.array(state.board))
-    print(state.snake)
+
+    import time
+
+    time.sleep(0.5)
+
+    for i in range(10):
+        state.step(choice(list(ACTIONS.values())))
+        print("\033c", end="")
+        print(np.array(state.board))
+        time.sleep(0.5)
