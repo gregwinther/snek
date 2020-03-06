1ca8fe1748ff1b288f0d86574c9bbc5f914ec2dc
Øyvind Sigmundson Schøyen <oyvindschoyen@gmail.com>
2020-03-06 11:58:33 +0100

    Add score to title

--- snake_game/gui.py
+++ snake_game/gui.py
@@ -5,10 +5,12 @@ import matplotlib.pyplot as plt
 class MatplotlibGui:
     def __init__(self, title="Snake game"):
         plt.figure()
-        plt.title(title)
+        self.title = title + " score: {0}"
+        plt.title(self.title.format(0))
 
     def render(self, state):
         plt.clf()
+        plt.title(self.title.format(state.score))
         plt.imshow(np.array(state.board))
         plt.pause(0.1)
         plt.draw()
